178e605ffec52b23e1e459c235276bfd46bf4eb8
Javier Gómez <alu0101049168@ull.edu.es>
2023-06-18 17:42:32 +0100

    Third commit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,7 +6,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField] bool isPlayer;
     [SerializeField] int health = 50;
-     [SerializeField] int score = 50;
+    [SerializeField] int score = 50;
     [SerializeField] ParticleSystem hitEffect;
 
     [SerializeField] bool applyCameraShake;
